66762564f337fd956c71b4e567a69fd499ea35c4
Al Company <albatoolcompany@Als-iMac.local>
2017-02-15 16:47:04 +0300

    Contact us form

--- Backgroundtest/ContactViewController.swift
+++ Backgroundtest/ContactViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ContactViewController: UIViewController {
 
+    @IBOutlet weak var emailButton: UIButton!
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        emailButton.layer.cornerRadius = 10
         navigationItem.title = "Contact Us"
         navigationController?.navigationBar.tintColor = UIColor(red: 141/255, green: 147/255, blue: 51/255, alpha: 1.0)
 
